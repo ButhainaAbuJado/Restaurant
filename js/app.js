1d921a494fc4ff6f0b16b14c8b12f9cab3a4c096
Buthaina Abu Jado <113937040+ButhainaAbuJado@users.noreply.github.com>
2023-06-03 18:27:31 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,36 +6,32 @@ function FoodForm(name, type, price){
     this.foodName = name;
     this.foodType= type;
     this.price = price;
-    this.generatingID =function(){ 
-        this.foodId=foodCid;
-        foodCid=foodCid+1;
-        return(this.foodId);
-   },
+
    allfood.push(this);
 };
 
-resForm.prototype.render = function () {
-    var id = this.generatingID();
-    var name = this.foodName;
-    var type = this.foodType;
-    var price = this.price;
-    var table = document.getElementById("tableData");
-    var rowCount = table.rows.length;
-    var row = table.insertRow(rowCount);
-    row.insertCell(0).innerHTML=id;
-    row.insertCell(1).innerHTML= name;
-    row.insertCell(2).innerHTML= type;
-    row.insertCell(3).innerHTML= price;
-   }
+Foodform.prototype.generatingID = function(){
+ this.foodId=foodCid;
+        foodCid=foodCid+1;
+        return(this.foodId);
+}
 
-  function handler(e){
+let saveValues = document.getElementById('resForm');
+  saveValues.addEventListener('submit', handler);
+
+    function handler(e){
     e.preventDefault();
     let Name =  e.target.name.value;
     let Type =  e.target.type.value;
     let price = e.target.price.value;
-    let food = new resForm(Name, Type, price);
-    food.render();
-    console.log(food);
+    let food = new FoodForm(Name, Type, price);
+      food.generatingID();
+  storeData();
   }
-  let saveValues = document.getElementById('FoodForm');
-  saveValues.addEventListener('submit', handler)
+  
+  
+  function storeData(){
+    let jsonArray = JSON.stringify(allfood);
+    window.localStorage.setItem("Food", jsonArray);
+    console.log(jsonArray);
+  }
